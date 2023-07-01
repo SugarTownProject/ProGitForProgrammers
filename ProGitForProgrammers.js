d1c43f22e1f4f7b63fdd636801fd9eb10a157038
Hanck1215 <st11025212@gmail.com>
2023-07-02 00:24:51 +0800

    Add the calculate branch.

--- ProGitForProgrammers.js
+++ ProGitForProgrammers.js
@@ -1,3 +1,4 @@
 console.log("Hello World!") ;
 console.log("I just added this in SugarTown virtual machine.") ;
 console.log("This line added to shwow the staging area.") ;
+console.log("I write this comment in branch.") ;
