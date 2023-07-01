9889c264b76951eb3fb104de585efe2d7aa99e43
Hanck1215 <st11025212@gmail.com>
2023-07-02 02:34:02 +0800

    I change the book's arrange.

--- ProGitForProgrammers.js
+++ ProGitForProgrammers.js
@@ -1,6 +1,5 @@
 console.log("Hello World!") ;
 console.log("I just added this in SugarTown virtual machine.") ;
-
 let name = function() { return "Github" ; }
-
 let date = function() { return "2023/12/15" ; }
+
