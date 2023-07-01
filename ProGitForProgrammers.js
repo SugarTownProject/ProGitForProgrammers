bb91d55c8646f2d5e8afca482b234a0ddb9bfa6f
Hanck1215 <st11025212@gmail.com>
2023-07-02 00:33:16 +0800

    Update a function, whose name is 'add'.It can combine two numbers.

--- ProGitForProgrammers.js
+++ ProGitForProgrammers.js
@@ -2,3 +2,5 @@ console.log("Hello World!") ;
 console.log("I just added this in SugarTown virtual machine.") ;
 console.log("This line added to shwow the staging area.") ;
 console.log("I write this comment in branch.") ;
+
+let add = function(num1, num2) {return num1 + num2 ; }
