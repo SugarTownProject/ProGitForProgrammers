4876d318697ac12eac2b937dec0ab9a26faf638f
Hanck1215 <st11025212@gmail.com>
2023-07-02 01:10:30 +0800

    Update a function, whose name is 'subtract'.It can let a number subtract another number.

--- ProGitForProgrammers.js
+++ ProGitForProgrammers.js
@@ -3,4 +3,6 @@ console.log("I just added this in SugarTown virtual machine.") ;
 console.log("This line added to shwow the staging area.") ;
 console.log("I write this comment in branch.") ;
 
-let add = function(num1, num2) {return num1 + num2 ; }
+let add = function(num1, num2) { return num1 + num2 ; }
+
+let subtract = function(num1, num2) { return num1 - num2 ; }
