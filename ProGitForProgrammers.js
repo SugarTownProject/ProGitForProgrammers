bbcbcd2872450f6238222922cfaf4fdeba3d4e31
Hanck1215 <st11025212@gmail.com>
2023-07-02 01:14:41 +0800

    Add multiplication function.

--- ProGitForProgrammers.js
+++ ProGitForProgrammers.js
@@ -6,3 +6,5 @@ console.log("I write this comment in branch.") ;
 let add = function(num1, num2) { return num1 + num2 ; }
 
 let subtract = function(num1, num2) { return num1 - num2 ; }
+
+let multiplication = function(num1, num2) { return num1* num2 ; }
